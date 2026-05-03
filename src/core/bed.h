8c78567aecd9c836811e73a2f0c4e3be4d9aecb6
samiunarno <samiunarno.iub@gmail.com>
2026-05-04 01:59:43 +0800

    New Update

--- src/core/bed.h
+++ src/core/bed.h
@@ -1,22 +1,24 @@
 #ifndef BED_H
 #define BED_H
-
 #include "../../include/common.h"
 
 typedef struct Bed {
+
     int bedNumber;
     BedStatus status;
     int patientId;
-    char patientName[MAX_NAME_LENGTH];
+    char patientName[100];
     struct Bed* next;
-} Bed;
+
+} Bed ;
+
 
 Bed* createBed(int bedNumber);
-void initializeBed(Bed* bed, int bedNumber);
-void occupyBed(Bed* bed, int patientId, const char* patientName);
+void initalizeBed(Bed* bed , int bedNumber);
+void occupyBed(Bed* bed , int patientId,const char* patientName);
 void vacateBed(Bed* bed);
-int isBedAvailable(const Bed* bed);
+
+int isBedAvailable(const Bed*bed);
 void displayBed(const Bed* bed);
 void freeBed(Bed* bed);
-
 #endif
